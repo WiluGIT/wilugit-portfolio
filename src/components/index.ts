ffacf0e9bdc36efd6341a5e2cfe9b60133181a11
Konrad Wilczek <konrad.wilczek@ttms.pl>
2023-05-25 01:16:14 +0200

    Finished basic portfolio UI

---
+++ src/components/index.ts
@@ -0,0 +1,25 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Experience from "./Experience";
+import Tech from "./Tech";
+import Works from "./Works";
+import Contact from "./Contact";
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import CanvasLoader from "./CanvasLoader";
+
+
+export {
+    Hero,
+    Navbar,
+    About,
+    Experience,
+    Tech,
+    Works,
+    Contact,
+    CanvasLoader,
+    EarthCanvas,
+    BallCanvas,
+    ComputersCanvas,
+    StarsCanvas
+};
